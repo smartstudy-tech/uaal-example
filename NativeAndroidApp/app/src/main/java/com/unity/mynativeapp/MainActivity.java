3b4e9b3cf06602190b1fe466161fbdbbb65d3ad8
superbDerrick <kang.derrick@gmail.com>
2021-05-20 20:59:48 +0900

    added test code more

--- NativeAndroidApp/app/src/main/java/com/unity/mynativeapp/MainActivity.java
+++ NativeAndroidApp/app/src/main/java/com/unity/mynativeapp/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //ss
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
